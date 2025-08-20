9aa798353002de40fc9082c8f733644da39dab4e
Kapil Khandelwal <42177808+thekapilkhandelwal@users.noreply.github.com>
2025-08-20 16:43:19 +0530

    Broadcast cache eviction across pods

--- src/main/java/com/example/users/UsersApplication.java
+++ src/main/java/com/example/users/UsersApplication.java
@@ -2,9 +2,11 @@ package com.example.users;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.kafka.annotation.EnableKafka;
 
 @EnableKafka
+@EnableCaching
 @SpringBootApplication
 public class UsersApplication {
     public static void main(String[] args) {
