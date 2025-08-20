cb08245156fac4119abc22ae484717792cd7bed1
Kapil Khandelwal <42177808+thekapilkhandelwal@users.noreply.github.com>
2025-08-20 16:15:38 +0530

    Implement basic user CRUD (#1)

---
+++ src/main/java/com/example/users/User.java
@@ -0,0 +1,15 @@
+package com.example.users;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Double salary;
+}
